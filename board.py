8637c65951dc8918a936ecf8056cc20c768cef3f
jialjohnson10 <jialjohnson@gmail.com>
2023-04-18 13:02:58 -0400

    beginning updates in Board - updated so all def are there added sudoku generator from the original github projected

--- board.py
+++ board.py
@@ -1,1 +1,40 @@
 #Jia
+
+class Cell:
+    def __init__(self,width,height,screen,difficulty):
+        self.width = width
+        self.height = height
+        self.difficult = difficulty 
+
+    def draw(self):
+        pass
+
+    def select(self,row,col):
+        pass
+
+    def click(self,x,y):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self):
+        pass
+
+    def place_numer(self,value):
+        pass
+
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def update_board(self):
+        pass
+
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
