e4d8ea17b4fdb6b42af7c8b60c3533879c5bce23
alejandraalzamora <a.alzamora@ufl.edu>
2023-04-18 13:27:30 -0400

    setting up methods

--- cell.py
+++ cell.py
@@ -1,1 +1,29 @@
 #Alejandra
+
+class Cell:
+    def __init__(self, value, row, col, screen):
+        # constructor for cell class
+        self.value = value
+        self.row = row
+        self.col = col
+
+        self.selected = False
+        pass
+
+    def set_cell_value(self, value):
+        # setter for this cell's value
+        pass
+
+    def set_sketched_value(self, value):
+        # setter for this cell's sketched value
+        pass
+
+    def draw(self):
+        # draws cell w value inside it
+        # if cell is not 0, value is displayed
+        # if cell has a 0 value, no value is displayed in cell
+        # cell is outlined red if it is currently selected
+        pass
+
+
+
