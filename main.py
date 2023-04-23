e25a85b0047719abd74aa0a4b498d3135998fa4f
Gatorxiwen <wuxiwen@ufl.edu>
2023-04-23 14:30:28 -0400

    xiwen edit pygame.K_KP_ENTER 5

--- main.py
+++ main.py
@@ -154,12 +154,12 @@ while True:  # window always showing in screen
                 if key.isdigit() and board.selected_cell:
                     board.set_cell_value(int(key))
 
-            if game_over:
-                pygame.display.update()
-                pygame.time.delay(3000)
-                generate_new_game()
-                game_over = False
-                board.draw()
+        if game_over:
+            pygame.display.update()
+            pygame.time.delay(3000)
+            generate_new_game()
+            game_over = False
+            board.draw()
 
     pygame.display.update()  # to display and update things on the screen
 ##
