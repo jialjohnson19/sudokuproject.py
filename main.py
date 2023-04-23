bbb03f779b664e5c321150d19f513712e9b92b78
alejandraalzamora <a.alzamora@ufl.edu>
2023-04-23 13:43:07 -0400

    game over screen

--- main.py
+++ main.py
@@ -67,6 +67,59 @@ def draw_game_start(screen):
         pygame.display.update()
 
 
+def draw_game_over(screen):
+    game_over_font = pygame.font.Font(None, 40)
+    button_font = pygame.font.Font(None, 70)
+    winner = False
+    screen.fill(BG_COLOR)
+    if winner:
+        text = f'Game Won!'
+        exit_text = button_font.render("EXIT", 0, (255, 255, 255))
+        # Initialize button background color and text
+        exit_surface = pygame.Surface((exit_text.get_size()[0] + 20,
+                                       exit_text.get_size()[1] + 20))
+        exit_surface.fill((255, 165, 0))
+        exit_surface.blit(exit_text, (10, 10))
+
+        # initialize button rectangle
+        exit_rectangle = exit_surface.get_rect(
+            center=(WIDTH // 2, HEIGHT // 2 + 50))
+        screen.blit(exit_surface, exit_rectangle)
+    else:
+        text = "Game Over :("
+        restart_text = button_font.render("RESTART", 0, (255, 255, 255))
+        # Initialize button background color and text
+        restart_surface = pygame.Surface((restart_text.get_size()[0] + 20,
+                                          restart_text.get_size()[1] + 20))
+        restart_surface.fill((255, 165, 0))
+        restart_surface.blit(restart_text, (10, 10))
+
+        # initialize button rectangle
+        restart_rectangle = restart_surface.get_rect(
+            center=(WIDTH // 2, HEIGHT // 2 + 50))
+        screen.blit(restart_surface, restart_rectangle)
+
+    game_over_surf = game_over_font.render(text, 0, LINE_COLOR)
+    game_over_rect = game_over_surf.get_rect(center=(WIDTH // 2, HEIGHT // 2 - 100))
+    screen.blit(game_over_surf, game_over_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if restart_rectangle.collidepoint(event.pos):
+                    # Checks if mouse is on start button
+                    return  # If the mouse is on the start button, we can return to main
+                elif exit_rectangle.collidepoint(event.pos):
+                    # If the mouse is on the quit button, exit the program
+                    sys.exit()
+        pygame.display.update()
+
+
+
+
+
 while True:  # window always showing in screen
     # event handler
     for event in pygame.event.get():
