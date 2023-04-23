d077e125c4ea310a863619410ea1648747f0a139
jialjohnson10 <jialjohnson@gmail.com>
2023-04-23 14:03:21 -0400

    game_start_screen update

--- main.py
+++ main.py
@@ -12,6 +12,65 @@ screen.fill(BG_COLOR)
 board = Board.draw(screen)
 winner = False
 
+def draw_game_start(screen):
+    # Initialize title font
+    start_title_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 70)
+
+    # Color background
+    screen.fill(BG_COLOR)
+
+    # Initialize and draw title
+    title_surface = start_title_font.render("Sudoku", 0, LINE_COLOR)
+    title_rectangle = title_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surface, title_rectangle)
+
+    # Initialize buttons
+    # Initialize text first
+    easy_text = button_font.render("Easy", 0, (255, 255, 255))
+    medium_text = button_font.render("Medium", 0, (255, 255, 255))
+    hard_text = button_font.render("Hard", 0, (255, 255, 255))
+
+    # Initialize button background color and text
+    easy_surface = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))
+    easy_surface.fill(LINE_COLOR)
+    easy_surface.blit(easy_text, (10, 10))
+    medium_surface = pygame.Surface((medium_text.get_size()[0] + 20, medium_text.get_size()[1] + 20))
+    medium_surface.fill(LINE_COLOR)
+    medium_surface.blit(medium_text, (10, 10))
+    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
+    hard_surface.fill(LINE_COLOR)
+    hard_surface.blit(hard_text, (10, 10))
+
+    # Initialize button rectangle
+    easy_rectangle = easy_surface.get_rect(
+        center=(WIDTH // 2 - 200 , HEIGHT // 2 ))
+    medium_rectangle = medium_surface.get_rect(
+        center=(WIDTH // 2 , HEIGHT // 2 ))
+    hard_rectangle = medium_surface.get_rect(
+        center=(WIDTH // 2 + 250, HEIGHT // 2))
+
+    # Draw buttons
+    screen.blit(easy_surface, easy_rectangle)
+    screen.blit(medium_surface, medium_rectangle)
+    screen.blit(hard_surface, hard_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if easy_rectangle.collidepoint(event.pos):
+                    # Checks if mouse is on start button
+                    return  # If the mouse is on the start button, we can return to main
+                elif medium_rectangle.collidepoint(event.pos):
+                    # If the mouse is on the quit button, exit the program
+                    pass
+                elif hard_rectangle.collidepoint(even.pos):
+                    pass
+        pygame.display.update()
+        
 def draw_game_over(screen):
     game_over_font = pygame.font.Font(None, 200)
     screen.fill(BG_COLOR)
