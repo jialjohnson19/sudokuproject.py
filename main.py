cba8ea5e896defc4f4b26ab1be3388d7febcc58f
jialjohnson10 <jialjohnson@gmail.com>
2023-04-23 13:38:40 -0400

    game_start_screen

--- main.py
+++ main.py
@@ -5,55 +5,61 @@ from board import Board
 from cell import Cell
 from constants import *
 
-def draw_game_start(screen):
-    # Initialize title font
+def draw_game_start(screen):  # shows the game start screen
+    # title font
     start_title_font = pygame.font.Font(None, 100)
     button_font = pygame.font.Font(None, 70)
 
-    # Color background
+    # background color
     screen.fill(BG_COLOR)
 
-    # Initialize and draw title
-    title_surface = start_title_font.render("Tic Tac Toe", 0, LINE_COLOR)
+    # initiliezes and draws the title
+    title_surface = start_title_font.render("Sudoku", 0, LINE_COLOR)
     title_rectangle = title_surface.get_rect(
         center=(WIDTH // 2, HEIGHT // 2 - 150))
     screen.blit(title_surface, title_rectangle)
 
-    # Initialize buttons
-    # Initialize text first
-    start_text = button_font.render("Start", 0, (255, 255, 255))
-    quit_text = button_font.render("Quit", 0, (255, 255, 255))
+    # initializes buttons text
+    easy_text = button_font.render("Easy", 0, (155, 155, 155))
+    medium_text = button_font.render("Medium", 0, (155, 155, 155))
+    hard_text = button_font.render("Hard", 0, (155, 155, 155))
 
-    # Initialize button background color and text
-    start_surface = pygame.Surface((start_text.get_size()[0] + 20, start_text.get_size()[1] + 20))
-    start_surface.fill(LINE_COLOR)
-    start_surface.blit(start_text, (10, 10))
-    quit_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
-    quit_surface.fill(LINE_COLOR)
-    quit_surface.blit(quit_text, (10, 10))
+    # initializes button background color and text
+    easy_surface = pygame.Surface((easy_text.get_size()[0] + 20, easy_text.get_size()[1] + 20))
+    easy_surface.fill(LINE_COLOR)
+    easy_surface.blit(easy_text, (10, 10))
+    medium_surface = pygame.Surface((medium_text.get_size()[0] + 20, medium_text.get_size()[1] + 20))
+    medium_surface.fill(LINE_COLOR)
+    medium_surface.blit(medium_text, (10, 10))
+    hard_surface = pygame.Surface((hard_text.get_size()[0] + 20, hard_text.get_size()[1] + 20))
+    hard_surface.fill(LINE_COLOR)
+    hard_surface.blit(hard_text, (10, 10))
 
-    # Initialize button rectangle
-    start_rectangle = start_surface.get_rect(
-        center=(WIDTH // 2, HEIGHT // 2 + 50))
-    quit_rectangle = quit_surface.get_rect(
-        center=(WIDTH // 2, HEIGHT // 2 + 150))
+    # initliaizes the rectange of the buttons
+    easy_rectangle = easy_surface.get_rect(
+        center=(WIDTH // 2 + 50, HEIGHT // 2))
+    medium_rectangle = medium_surface.get_rect(
+        center=(WIDTH // 2 + 150, HEIGHT // 2))
+    hard_rectangle = hard_surface.get_rect(
+        center=(WIDTH // 2 + 200, HEIGHT // 2))
 
-    # Draw buttons
-    screen.blit(start_surface, start_rectangle)
-    screen.blit(quit_surface, quit_rectangle)
+    # draws the rectangles
+    screen.blit(easy_surface, easy_rectangle)
+    screen.blit(medium_surface, medium_rectangle)
+    screen.blit(hard_surface, hard_rectangle)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if start_rectangle.collidepoint(event.pos):
-                    # Checks if mouse is on start button
-                    return  # If the mouse is on the start button, we can return to main
-                elif quit_rectangle.collidepoint(event.pos):
-                    # If the mouse is on the quit button, exit the program
-                    sys.exit()
-        pygame.display.update()
+                if easy_rectangle.collidepoint(event.pos):
+                    pass
+                if medium_rectangle.collidepoint(event.pos):
+                    pass
+                if hard_rectangle.collidepoint(event.pos):
+                    pass
+        pygame.display.update
 
 
 pygame.init()
