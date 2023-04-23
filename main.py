9e64c796c2cac5c18bdea7913ac7a6f9ea1c4d0f
jialjohnson10 <jialjohnson@gmail.com>
2023-04-23 12:58:21 -0400

    main updated

--- main.py
+++ main.py
@@ -14,74 +14,9 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(BG_COLOR)
 
 
-def draw(self):  # draws the lines for the board
-    # horizontal lines?
-    for i in range(1, 3):
-        pygame.draw.line(screen,
-                         LINE_COLOR,
-                         (0, i * SQUARE_SIZE),
-                         (WIDTH, i * SQUARE_SIZE),
-                         LINE_WIDTH)
-    for i in range(3, 4):
-        pygame.draw.line(screen,
-                         LINE_COLOR,
-                         (0, i * SQUARE_SIZE),
-                         (WIDTH, i * SQUARE_SIZE),
-                         LINE_WIDTH_BIG)
-    for i in range(4, 6):
-        pygame.draw.line(screen,
-                         LINE_COLOR,
-                         (0, i * SQUARE_SIZE),
-                         (WIDTH, i * SQUARE_SIZE),
-                         LINE_WIDTH)
-    for i in range(6, 7):
-        pygame.draw.line(screen,
-                         LINE_COLOR,
-                         (0, i * SQUARE_SIZE),
-                         (WIDTH, i * SQUARE_SIZE),
-                         LINE_WIDTH_BIG)
-    for i in range(7, 9):
-        pygame.draw.line(screen,
-                         LINE_COLOR,
-                         (0, i * SQUARE_SIZE),
-                         (WIDTH, i * SQUARE_SIZE),
-                         LINE_WIDTH)
-
-        for i in range(1, 3):
-            pygame.draw.line(screen,
-                             LINE_COLOR,
-                             (SQUARE_SIZE * i, 0),
-                             (SQUARE_SIZE * i, HEIGHT),
-                             LINE_WIDTH)
-        for i in range(3, 4):
-            pygame.draw.line(screen,
-                             LINE_COLOR,
-                             (SQUARE_SIZE * i, 0),
-                             (SQUARE_SIZE * i, HEIGHT),
-                             LINE_WIDTH_BIG)
-        for i in range(4, 6):
-            pygame.draw.line(screen,
-                             LINE_COLOR,
-                             (SQUARE_SIZE * i, 0),
-                             (SQUARE_SIZE * i, HEIGHT),
-                             LINE_WIDTH)
-        for i in range(6, 7):
-            pygame.draw.line(screen,
-                             LINE_COLOR,
-                             (SQUARE_SIZE * i, 0),
-                             (SQUARE_SIZE * i, HEIGHT),
-                             LINE_WIDTH_BIG)
-        for i in range(7, 9):
-            pygame.draw.line(screen,
-                             LINE_COLOR,
-                             (SQUARE_SIZE * i, 0),
-                             (SQUARE_SIZE * i, HEIGHT),
-                             LINE_WIDTH)
-
-draw()
+board = Board.draw(screen) #board is drawn from here
 c1 = Cell(1, 1, 0, screen)
 c1.draw()
-
 
 while True:  # window always showing in screen
     # event handler
