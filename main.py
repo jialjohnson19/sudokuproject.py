5c3ce37f684a3356eb539a40559d532cb244be0c
jialjohnson10 <jialjohnson@gmail.com>
2023-04-23 13:34:58 -0400

    created game_start_screen

--- main.py
+++ main.py
@@ -13,9 +13,56 @@ font = pygame.font.Font(None, 40)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(BG_COLOR)
 
-
 board = Board.draw(screen) #board is drawn from here
+def draw_game_start(screen):
+    # Initialize title font
+    start_title_font = pygame.font.Font(None, 100)
+    button_font = pygame.font.Font(None, 70)
 
+    # Color background
+    screen.fill(BG_COLOR)
+
+    # Initialize and draw title
+    title_surface = start_title_font.render("Tic Tac Toe", 0, LINE_COLOR)
+    title_rectangle = title_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 - 150))
+    screen.blit(title_surface, title_rectangle)
+
+    # Initialize buttons
+    # Initialize text first
+    start_text = button_font.render("Start", 0, (255, 255, 255))
+    quit_text = button_font.render("Quit", 0, (255, 255, 255))
+
+    # Initialize button background color and text
+    start_surface = pygame.Surface((start_text.get_size()[0] + 20, start_text.get_size()[1] + 20))
+    start_surface.fill(LINE_COLOR)
+    start_surface.blit(start_text, (10, 10))
+    quit_surface = pygame.Surface((quit_text.get_size()[0] + 20, quit_text.get_size()[1] + 20))
+    quit_surface.fill(LINE_COLOR)
+    quit_surface.blit(quit_text, (10, 10))
+
+    # Initialize button rectangle
+    start_rectangle = start_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 50))
+    quit_rectangle = quit_surface.get_rect(
+        center=(WIDTH // 2, HEIGHT // 2 + 150))
+
+    # Draw buttons
+    screen.blit(start_surface, start_rectangle)
+    screen.blit(quit_surface, quit_rectangle)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_rectangle.collidepoint(event.pos):
+                    # Checks if mouse is on start button
+                    return  # If the mouse is on the start button, we can return to main
+                elif quit_rectangle.collidepoint(event.pos):
+                    # If the mouse is on the quit button, exit the program
+                    sys.exit()
+        pygame.display.update()
 
 while True:  # window always showing in screen
     # event handler
