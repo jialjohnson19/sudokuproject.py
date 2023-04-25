2aa74dd7faaf3e76832c35ef924e6610b5460dc7
alejandraalzamora <116974598+alejandraalzamora@users.noreply.github.com>
2023-04-25 14:38:07 -0400

    update cell.py

--- cell.py
+++ cell.py
@@ -1,8 +1,7 @@
-# Alejandr
+# Alejandra
 
 import pygame
 from constants import *
-
 
 pygame.init()
 
@@ -12,13 +11,7 @@ screen.fill(BG_COLOR)
 
 class Cell:
 
-    # def __init__(self, value, row, col, screen):
-    #     self.width = screen.width
-    #     self.height = screen.height
-    #     etc
-
-
-    def __init__(self, value, row, col,screen):
+    def __init__(self, value, row, col, screen):
         # constructor for cell class
         # screen is a window from pycharm
         self.value = value
@@ -27,6 +20,16 @@ class Cell:
         self.col = col
         self.screen = screen
         self.selected = False
+        self.event = None
+
+    def is_selected(self):
+        return self.selected
+
+    def get_value(self):
+        return self.value
+
+    def selected(self):
+        self.selected = True
 
     def set_cell_value(self, value):
         # setter for this cell's value
@@ -37,25 +40,30 @@ class Cell:
         self.sketched_value = value
 
     def draw(self):
+
         # draws cell w value inside it
         # defining the text and number values
         num_font = pygame.font.Font(None, 90)
+        num_surf = num_font.render(str(self.value), 0, NUM_COLOR)
 
-        num_1_surf = num_font.render('1', 0, NUM_COLOR)
-        num_2_surf = num_font.render('2', 0, NUM_COLOR)
-        num_3_surf = num_font.render('3', 0, NUM_COLOR)
-        num_4_surf = num_font.render('4', 0, NUM_COLOR)
-        num_5_surf = num_font.render('5', 0, NUM_COLOR)
-        num_6_surf = num_font.render('6', 0, NUM_COLOR)
-        num_7_surf = num_font.render('7', 0, NUM_COLOR)
-        num_8_surf = num_font.render('8', 0, NUM_COLOR)
-        num_9_surf = num_font.render('9', 0, NUM_COLOR)
+        if self.value > 0:
+            num_rect = num_surf.get_rect(
+                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
+                        SQUARE_SIZE + SQUARE_SIZE // 2))
 
-        if self.selected:
+            self.screen.blit(num_surf, num_rect)
+
+        elif self.is_selected() and self.value > 0:
             pygame.draw.rect(screen, (255, 0, 0),
                              pygame.Rect(self.col * SQUARE_SIZE, self.row *
                                          SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE), 12)
             self.selected = False
+
+            num_rect = num_surf.get_rect(
+                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
+                        SQUARE_SIZE + SQUARE_SIZE // 2))
+
+            self.screen.blit(num_surf, num_rect)
 
         if self.sketched_value is not None:
             sketched_font = pygame.font.Font(None, 20)
@@ -64,90 +72,10 @@ class Cell:
                 topleft=(self.col * SQUARE_SIZE + 5, self.row * SQUARE_SIZE + 5))
             self.screen.blit(sketched_surf, sketched_rect)
 
-        elif self.value == 1:
-            # define the location
-            num_1_rect = num_1_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_1_surf, num_1_rect)
-
-        elif self.value == 2:
-            # define the location
-            num_2_rect = num_2_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_2_surf, num_2_rect)
-
-        elif self.value == 3:
-            # define the location
-            num_3_rect = num_3_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_3_surf, num_3_rect)
-
-        elif self.value == 4:
-            # define the location
-            num_4_rect = num_4_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_4_surf, num_4_rect)
-
-        elif self.value == 5:
-            # define the location
-            num_5_rect = num_5_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_5_surf, num_5_rect)
-
-        elif self.value == 6:
-            # define the location
-            num_6_rect = num_6_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_6_surf, num_6_rect)
-
-        elif self.value == 7:
-            # define the location
-            num_7_rect = num_7_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_7_surf, num_7_rect)
-
-        elif self.value == 8:
-            # define the location
-            num_8_rect = num_8_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_8_surf, num_8_rect)
-
-        elif self.value == 9:
-            # define the location
-            num_9_rect = num_9_surf.get_rect(
-                center=(self.col * SQUARE_SIZE + SQUARE_SIZE // 2, self.row *
-                        SQUARE_SIZE + SQUARE_SIZE // 2))
-
-            # blit chip onto the screen
-            self.screen.blit(num_9_surf, num_9_rect)
-
         # call draw value function
         # if cell is not 0, value is displayed
         # if cell has a 0 value, no value is displayed in cell
         # is outlined red if it is currently selected
+
 
 
